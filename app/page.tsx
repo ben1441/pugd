5afb7618e3c1750ed2f7e005a6d86fe5bb5e4ca6
ben1441 <manovah04@gmail.com>
2023-11-09 21:41:06 +0530

    buttons positioning

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,19 @@ export default function Home() {
     <div className="flex flex-col items-start justify-center ml-10 sm:ml-32 lg:ml-40 gap-6">
       <h1 className="wagmi-gradient font-bold min-[320px]:text-[80px] sm:text-[100px]">Wagmi</h1>
       <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elitt</p>
-      <Link href="#more">
-        <Image src="/learn.svg" alt="Learn More" width={200} height={100} />
-      </Link>
-      <Link href="#getStarted">
-        <Image src="/get-started.svg" alt="Learn More" width={200} height={100} />
-      </Link>
+      <div className="flex max-sm:flex-col gap-8">
+        <Link href="#more">
+          <button className="text-white font-bold btn-gradient-purple w-32 h-10">Get Started</button>
+        </Link>
+        <Link href="#getStarted">
+          <button className="text-white font-bold btn-gradient-light w-32 h-10">Learn More</button>
+        </Link>
+      </div>
     </div>
 
     {/*<Image src="/wagmi-bg.png" alt="wagmi" width={600} height={0} />*/}
-    <div className="fixed -top-[20%] -left-[15%] soft-circle w-[400px] lg:w-[581px] h-[400px] lg:h-[609px] flex-shrink-0"></div>
-    <div className="fixed top-[60%] left-[65%] sm:left-[75%] md:left-[85%] soft-circle w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] flex-shrink-0"></div>
-    <Image src="/wagmi-pose.png" alt="wagmi" width={200} height={200} className="fixed z-behind bottom-0 right-0" />
+    <div className="relative -top-[40%] md:-left-[] lg:-left-[55%] soft-circle w-[400px] lg:w-[581px] h-[400px] lg:h-[609px] flex-shrink-0"></div>
+    <div className="relative soft-circle w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] flex-shrink-0"></div>
+    {/*<Image src="/wagmi-pose.png" alt="wagmi" width={200} height={200} className="fixed z-behind bottom-0 right-0" />*/}
   </div>;
 }
