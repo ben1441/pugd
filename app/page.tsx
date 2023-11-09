e33313c8501ca725732e86fc0cb5c08e874ad379
ben1441 <manovah04@gmail.com>
2023-11-09 21:59:03 +0530

    bg positioning 4

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
     </div>
 
     {/*<Image src="/wagmi-bg.png" alt="wagmi" width={600} height={0} />*/}
-    <div className="fixed z-behind -top-[40%] md:-left-[50%] lg:-left-[55%] soft-circle w-[400px] lg:w-[581px] h-[400px] lg:h-[609px] flex-shrink-0"></div>
-    <div className="fixed -z-10 soft-circle w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] flex-shrink-0"></div>
+    <div className="fixed -top-[40%] -left-[30%] md:-left-[50%] lg:-left-[55%] soft-circle w-[400px] lg:w-[581px] h-[400px] lg:h-[609px] flex-shrink-0"></div>
+    <div className="fixed bottom-[10%] -right-[20%] soft-circle w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] flex-shrink-0"></div>
     {/*<Image src="/wagmi-pose.png" alt="wagmi" width={200} height={200} className="fixed z-behind bottom-0 right-0" />*/}
   </div>;
 }
