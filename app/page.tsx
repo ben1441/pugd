bad00e940011fd5d0c52903631a94ce05cd9425e
ben1441 <manovah04@gmail.com>
2023-11-10 20:40:19 +0530

    hero alter 4

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,20 @@ export default function Home() {
       {/*  <Image src="/balloon.svg" alt="balloon" width={200} height={200} className="relative left-5" />*/}
       {/*  <Image src="/balloon.svg" alt="balloon" width={200} height={200} className="relative left-7" />*/}
       {/*</div>*/}
-      <div className="flex flex-col items-start justify-center ml-6 min-[480px]:ml-10 sm:ml-32 lg:ml-40 gap-6">
-        <h1 className="wagmi-gradient mt-28 font-bold min-[320px]:text-[80px] sm:text-[100px]">Wagmi</h1>
-        <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elitt</p>
-        <div className="flex max-sm:gap-6 gap-8">
-          <Link href="#more">
-            <button className="text-white font-bold btn-gradient-purple w-32 h-10">Get Started</button>
-          </Link>
-          <Link href="#getStarted">
-            <button className="text-white font-bold btn-gradient-light w-32 h-10">Learn More</button>
-          </Link>
+      <div className="flex flex-col items-start justify-center gap-6">
+        <h1 className="wagmi-gradient mt-28 font-bold ml-6 min-[480px]:ml-10 sm:ml-32 lg:ml-40 min-[320px]:text-[80px] sm:text-[100px]">Wagmi</h1>
+        <p className="text-white ml-6 min-[480px]:ml-10 sm:ml-32 lg:ml-40">Lorem ipsum dolor sit amet, consectetur adipiscing elitt</p>
+        <div className="flex max-sm:flex-col max-sm:gap-8 items-start justify-end md:justify-between w-screen">
+          <div className="flex max-sm:gap-6 ml-8 md:ml-32 gap-8">
+            <Link href="#more">
+              <button className="text-white font-bold btn-gradient-purple w-32 h-10">Get Started</button>
+            </Link>
+            <Link href="#getStarted">
+              <button className="text-white font-bold btn-gradient-light w-32 h-10">Learn More</button>
+            </Link>
+          </div>
+          <Image src="/wagmi-hero.png" alt="Wagmi" width={500} height={200} />
         </div>
-        <Image src="/wagmi-hero.png" alt="Wagmi" width={500} height={200} className="max-md:mt-8 lg:absolute lg:bottom-0 lg:right-0" />
       </div>
     </div>
     <Roadmap />
