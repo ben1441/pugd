af101ccaee1c3ecfa38752a822e229ba8b8bcbd3
ben1441 <manovah04@gmail.com>
2023-11-10 18:06:46 +0530

    alter roadmap 1

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Roadmap = () => {
     return (
         <div className="flex flex-col gap-6 bg-[#9A6AEC] items-center justify-start w-screen min-h-screen h-auto">
-            <h1 className="wagmi-gradient mt-28 font-bold min-[320px]:text-[80px] sm:text-[100px]">Roadmap</h1>
+            <h1 className="wagmi-gradient mt-28 font-bold text-6xl min-[360px]:text-7xl sm:text-8xl">Roadmap</h1>
             <Image src="/roadmap.svg" alt="Roadmap" width={1350} height={200} className="max-md:hidden" />
             <Image src="/roadmap_mobile.svg" alt="Roadmap" width={300} height={200} className="md:hidden" />
         </div>
