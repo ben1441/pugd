6a562563ba8395ce30a75e62de4322215b601ff1
Ben <manovah04@gmail.com>
2024-06-01 06:33:45 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,16 +9,16 @@ const Footer = () => {
       className="bg-[#999999] flex flex-col gap-4 items-center justify-center border-t-8 border-[#ff9564] w-screen h-40"
     >
       <div className="flex items-center justify-center w-auto h-20 rounded-full bg-white/20 backdrop-blur morphism">
-        <div className="m-2 w-16">
-          <Link href="https://x.com/pughopiuminu">
+        <Link className="m-2 w-16" href="https://x.com/pughopiuminu">
+          <div>
             <Player src="/twitter.json" loop autoplay />
-          </Link>
-        </div>
-        <div className="w-[90px]">
-          <Link href="https://t.me/officialPugo">
+          </div>
+        </Link>
+        <Link className="w-[90px]" href="https://t.me/PugOnHopium">
+          <div>
             <Player src="/telegram.json" loop autoplay />
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
       <div className="flex items-center justify-center text-[#93563a]">
         <p>&copy; PugHopiumInu. 2024 All right reserved.</p>
